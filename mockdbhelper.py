36a9a6e49ca6a473de13c733d4eb1cf2367bdc68
saurek <s.reklaitis@gmail.com>
2018-04-24 16:28:32 +0300

    added mockdb, inouts fix

---
+++ mockdbhelper.py
@@ -0,0 +1,12 @@
+class MockDBHelper:
+    def connect(self):
+        pass
+
+    def get_all_inputs(self):
+        pass
+
+    def add_input(self, data):
+        pass
+
+    def clear(self):
+        pass
